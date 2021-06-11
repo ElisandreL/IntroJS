ef2ccc58c045b83c4c9ec9daba490878f638c52c
Elisandre <elisandre.laenens@gmail.com>
2021-06-11 23:04:10 +0200

    4 nifi

---
+++ Intro_4/4_6.js
@@ -0,0 +1,12 @@
+const readlineSync = require('readline-sync');
+// Exo 4.6
+let a = new Number(readlineSync.question('Choose a number : '));
+
+function factorial(a){
+    if(a == 0 || a == 1){
+        return 1;
+    }else{
+        return a * factorial(a-1);
+    }
+}
+console.log("The factorial of " + a + " is " + factorial(a));
